3e10a65bafdc36001466c9764f09f11d26ba61eb
Sanjar <115815477+lifeofsanjar@users.noreply.github.com>
2023-10-09 12:34:39 +0500

    Add files via upload

---
+++ homework2/main.js
@@ -0,0 +1,185 @@
+//1-masala
+
+// const dollar = 11000.34
+// const euro = 12354.03
+// const travel_expenses = 500 * dollar 
+// const hotel_fee = 250 * dollar
+// const entertaiment_fee = 120 * euro
+// let total_fee = parseInt(travel_expenses) + parseInt(hotel_fee) + parseInt(entertaiment_fee)
+// let sum = parseInt(prompt("Sayohatga chiqish uchun qancha mablag'izngiz bor?"))
+// if( sum == total_fee || sum > total_fee)
+// {
+//     alert("Oq yo'l")
+// }
+// else
+// {
+//     alert("Pul mablag'izngiz yetarli emas. Kamida " + total_fee + " so'm miqdorda pul mablag'izngiz bo'lishi kerak!")
+// }
+
+//if1
+
+// let a = parseFloat(prompt("sonni kiriting:"))
+// if( a>0) 
+// {
+//     ++a;
+// }
+// alert(a)
+
+//if2
+
+// let a = parseFloat(prompt("sonni kiriting:"))
+// if( a>0) 
+// {
+//     ++a;
+// }
+// else
+// {
+//     a=a-2
+// }
+// alert(a)
+
+//if3
+
+// let a = parseFloat(prompt("sonni kiriting:"))
+// if( a > 0) 
+// {
+//     ++a;
+// }
+// else if ( a==0 ) 
+// {
+//     a=10    
+// }
+// else
+// {
+//     a=a-2
+// }
+// alert(a)
+
+//if4
+
+// let a,n=0;
+// for(let i=0;i<3;i++)
+// {
+//     a = parseFloat(prompt("sonni kiriting:"));
+//     if( a>0 ) { n++ };
+// }
+// alert(n);
+
+//if5
+
+// let a,pos=0,neg=0;
+// for( let i = 0 ; i < 5 ; i++)
+// {
+//     a = parseFloat(prompt("sonni kiriting:"));
+//     if( a>0 ) { pos++ }
+//     else { neg++ }
+// }
+// alert("musbat "+pos +"  manfiy "+neg);
+
+//if6
+
+// let a,b
+// a=parseFloat(prompt("ani kiriting"))
+// b=parseFloat(prompt("bni kiriting"))
+// if(a>b)
+// {
+//     alert(a)
+// }
+// else
+// {
+//     alert(b)
+// }
+
+//if7
+
+// let a,b
+// a=parseFloat(prompt("ani kiriting"))
+// b=parseFloat(prompt("bni kiriting"))
+// if(a>b)
+// {
+//     alert("2-chi")
+// }
+// else
+// {
+//     alert("1-chi")
+// }
+
+//if8 
+
+// let a,b
+// a=parseFloat(prompt("ani kiriting"))
+// b=parseFloat(prompt("bni kiriting"))
+// if( a > b )
+// {
+//     alert(a + "  "+ b)
+// }
+// else
+// {
+//     alert(b + "  "+ a)
+// }
+
+//boolean1
+
+// let a , b=true
+// a=parseFloat(prompt("A soni musbat (ani kiriting)"))
+// if( a > 0)
+// {
+//     alert(b)
+// }
+// else
+// {
+//     alert(!b)
+// }
+
+//boolean2
+
+// let a , b, t=true
+// a=parseFloat(prompt("A>2 and B<=3 (ani kiriting)"))
+// b=parseFloat(prompt("A>2 and B<=3 (bni kiriting)"))
+// if( a > 2 && b<=3 )
+// {
+//     alert(t)
+// }
+// else
+// {
+//     alert(!t)
+// }
+
+//boolean17
+
+
+// let a , t=true
+// a=parseFloat(prompt(" bu son juft (ani kiriting)"))
+// if( a%2==0 )
+// {
+//     alert(t)
+// }
+// else
+// {
+//     alert(!t)
+// }
+
+//boolean18
+
+
+// let a , b , c , t=true
+// a=parseFloat(prompt(" hech bo`lmaganda 2 bir biriga teng (ani kiriting)"))
+// b=parseFloat(prompt(" hech bo`lmaganda 2 bir biriga teng (bni kiriting)"))
+// c=parseFloat(prompt(" hech bo`lmaganda 2 bir biriga teng (cni kiriting)"))
+// if( a==b )
+// {
+//     alert(t)
+// }
+// else if( b==c )
+// {
+//     alert(t)
+// }
+// else if( a==c )
+// {
+//     alert(t)
+// }
+// else
+// {
+//     alert(!t)
+// }
+
